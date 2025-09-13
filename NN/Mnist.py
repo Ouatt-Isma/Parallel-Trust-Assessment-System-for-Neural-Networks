f9064b7d8d89abdbc67b3b278af264ffc7f80ab0
Ouatt-Isma <ouattaraismael1999@gmail.com>
2025-09-13 14:57:04 +0200

    jj

--- NN/Mnist.py
+++ NN/Mnist.py
@@ -29,20 +29,20 @@ def main_pois(patch, plot=False):
     y_train_one_hot = encoder.fit_transform(y_train.reshape(-1, 1))
     y_test_one_hot = encoder.transform(y_test.reshape(-1, 1))
 
-    pois_X_test_6 = np.empty_like(X_test)
-    pois_X_test_3 = np.empty_like(X_test)
     ids_6 = np.where(np.argmax(y_test_one_hot, axis=1)==6)
     ids_3 = np.where(np.argmax(y_test_one_hot, axis=1)==3)
+    pois_X_test_6 = np.array([add_trigger_patch(X_test[i], patch_size=patch) for i in ids_6[0]])
+    pois_X_test_3 = np.array([add_trigger_patch(X_test[i], patch_size=patch) for i in ids_3[0]])
 
     print(f"Number of 6:: {np.shape(ids_6)[1]}")
     print(f"Number of 3:: {np.shape(ids_3)[1]}")
 
-    for i in ids_6[0]:
+    #for i in ids_6[0]:
         # pois_X_test_6[i] = apply_background_color(X_test[i]).reshape(-1)
-        pois_X_test_6[i] = add_trigger_patch(X_test[i], patch_size=patch)
-    for i in ids_3[0]:
+        #pois_X_test_6[i] = add_trigger_patch(X_test[i], patch_size=patch)
+    #for i in ids_3[0]:
         # pois_X_test_3[i] = apply_background_color(X_test[i]).reshape(-1)
-        pois_X_test_3[i] = add_trigger_patch(X_test[i], patch_size=patch)
+        #pois_X_test_3[i] = add_trigger_patch(X_test[i], patch_size=patch)
 
     # Create neural network
     nn = NeuralNetwork(input_size, hidden_size, output_size, ptas=True)
@@ -54,7 +54,7 @@ def main_pois(patch, plot=False):
         nn.ptas = False
         nn.train(X_train, y_train_one_hot, X_test=X_test, y_test= y_test_one_hot, 
                  epochs=10, batch_size=18, learning_rate=0.001, plot=True, X_non_pois_3=X_test[ids_3], 
-                 X_non_pois_6=X_test[ids_6], X_pois_3=pois_X_test_3, X_pois_6=pois_X_test_3, fname =f'MNIST{patch}')
+                 X_non_pois_6=X_test[ids_6], X_pois_3=pois_X_test_3, X_pois_6=pois_X_test_6, fname =f'MNIST{patch}')
     else:
         # nn.ptas = False
         nn.train(X_train, y_train_one_hot, epochs=10, batch_size=18, learning_rate=0.001)
@@ -101,16 +101,7 @@ def main_pois(patch, plot=False):
     # predictions = nn.predict(pois_X_test[ids_6[0][2]])
     # print(predictions)
 
-    accuracy = np.mean(pois_predictions == np.argmax(y_test_one_hot[ids_6], axis=1))
-    print(f"Accuracy on poisoned test 6: {accuracy * 100:.2f}%")
-
-    accuracy = np.mean(predictions == np.argmax(y_test_one_hot[ids_6], axis=1))
-    print(f"Test Accuracy on non poisoned test 6:: {accuracy * 100:.2f}%")
-
-    predictions = nn.predict(X_test[ids_3])
-    accuracy = np.mean(predictions == np.argmax(y_test_one_hot[ids_3], axis=1))
-    print(f"Test Accuracy on 3:: {accuracy * 100:.2f}%")
-
+  
 def test_size(plot=False):
     # Load dataset
     # X_train, X_test, y_train_one_hot, y_test_one_hot, _ = load_mnist()
@@ -237,7 +228,7 @@ def test():
 
 import time 
 def test_pois():
-    for patch in [20, 27]:
+    for patch in [1, 4, 20, 27]:
         print("New Training")
         
         main_pois(patch, plot=True)
